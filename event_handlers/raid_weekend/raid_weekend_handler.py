c209e8f1920523a69ce3e27051f2c303f23d1894
funzip <19338376+Fuzznip@users.noreply.github.com>
2025-07-18 10:58:22 -0700

    raid weekend and rank applications (untested) (#98)

---
+++ event_handlers/raid_weekend/raid_weekend_handler.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timezone
+from app import db
+from event_handlers.event_handler import EventSubmission, NotificationResponse, NotificationAuthor
+from models.models import Events
+from helper.jsonb import update_jsonb_field
+import random
+
+whitelist = [
+    "twisted bow",
+    "scythe of vitur (uncharged)",
+    "sanguinesti staff (uncharged)",
+    "averneic defender hilt",
+    "justiciar legguards",
+    "ghrazi rapier",
+    "justiciar chestguard",
+    "justiciar faceguard",
+    "dexterous prayer scroll",
+    "arcane prayer scroll",
+    "twisted buckler",
+    "dragon hunter crossbow",
+    "dinh's bulwark",
+    "ancestral hat",
+    "ancestral robe top",
+    "ancestral robe bottom",
+    "dragon claws",
+    "elder maul",
+    "kodai insignia",
+    "twisted bow",
+    "osmumten's fang",
+    "lightbearer",
+    "elidinis' ward",
+    "masori mask",
+    "masori body",
+    "masori chaps",
+    "tumeken's shadow (uncharged)"
+]
+
+def raid_weekend_handler(submission: EventSubmission) -> list[NotificationResponse]:
+    # Grab the most recent 'Boss of the Week' event
+    now = datetime.now(timezone.utc)
+    event = Events.query.filter(
+        Events.start_time <= now, 
+        Events.end_time >= now, 
+        Events.type == "RAID_WEEKEND"
+    ).first()
+
+    if event is None:
+        return None
+
+    if submission.type not in ["LOOT"]:
+        return None
+
+    if submission.type == "LOOT":
+        if submission.trigger.lower() in whitelist:
+            return [NotificationResponse(
+                threadId=event.thread_id,
+                title=f"{submission.rsn} has received a {submission.trigger}!",
+                color=0xFF0055,
+                thumbnailImage="https://media.discordapp.net/attachments/1393688970298265661/1393688970457780295/image.png?ex=687b55c0&is=687a0440&hm=128c7766e8f3df069364b383c829f6f99143e5f25bd43162607a0d456beb75b8&=&format=webp&quality=lossless",
+                author=NotificationAuthor(name=f"{submission.rsn}"),
+                description=f"Money"
+            )]
+
+    return None
