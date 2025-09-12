d9bc5652f1db555b0230c2158ce731ff88cd4b03
Fuzznip <fuzznip@gmail.com>
2025-09-12 11:59:12 -0700

    wwwwhoooooopppssssss

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 # Needed to auto generate tables using SQLAlchemy
-from models import models, stability_party_3
+from models import models, stability_party_3, bingo
 
 # make app aware of all endpoints
 from endpoints import users, announcements, splits, applications, diary, ranks, raid_tier, discord_management
