d5f4584df066a2586e3dcb2c640a10b41e9f7136
funzip <19338376+Fuzznip@users.noreply.github.com>
2025-09-14 15:57:10 -0700

    Bingus (#132)
    * catching errors on a handler level to prevent other working handlers from finishing their event handling
    
    * ok this might actually work
    
    * removed print debugs

--- endpoints/events/bingo.py
+++ endpoints/events/bingo.py
@@ -1,6 +1,7 @@
 from app import app, db
 from flask import request, jsonify
-from models.models import Events, EventTeams
+from models.bingo import BingoChallenges, BingoTiles
+from models.models import EventChallenges, EventTasks, EventTriggers, Events, EventTeams
 from datetime import datetime, timezone
 from helper.helpers import ModelEncoder
 import json
@@ -35,5 +36,83 @@ def get_bingo_board():
     if event is None:
         return jsonify({"error": "No active Bingo event found"}), 404
     
+    # Loop through each bingo tile
+    bingo_tiles: list[BingoTiles] = BingoTiles.query.filter_by(event_id=event.id).all()
+    if not bingo_tiles:
+        return jsonify({"error": "No bingo tiles found for the Bingo event"}), 404
+    
+    response = {"board": []}
+
+    for tile in bingo_tiles:
+        response["board"].append({
+            "name": tile.name,
+            "index": tile.index,
+            "tasks": []
+        })
+
+        # Get the challenges for the tile
+        tile_tasks: list[BingoChallenges] = BingoChallenges.query.filter_by(tile_id=tile.id).all()
+        if len(tile_tasks) > 1:
+            for i, task in enumerate(tile_tasks):
+                challenges_list: list[list[str]] = []
+                for challenge_id in task.challenges:
+                    challenge = EventChallenges.query.filter_by(id=challenge_id).first()
+                    if challenge is None:
+                        logging.error(f"Challenge {challenge_id} not found for bingo task {task.id}.")
+                        continue
+
+                    triggers = []
+                    for task_id in challenge.tasks:
+                        event_task: EventTasks = EventTasks.query.filter_by(id=task_id).first()
+                        if event_task is None:
+                            logging.error(f"Event task {task_id} not found for challenge {challenge_id}.")
+                            continue
+                        for trigger_id in event_task.triggers:
+                            event_trigger: EventTriggers = EventTriggers.query.filter_by(id=trigger_id).first()
+                            if event_trigger is None:
+                                logging.error(f"Event trigger {trigger_id} not found for task {task_id}.")
+                                continue
+                            triggers.append(event_trigger.trigger)
+                    
+                    challenges_list.append(triggers)
+
+                response["board"][-1]["tasks"].append({
+                    "task": f"Task {i+1}",
+                    "required": challenge_id,
+                    "id": str(task.id),
+                    "triggers": challenges_list
+                })
+        elif len(tile_tasks) == 1:
+            task = tile_tasks[0]
+            challenges_list: list[list[str]] = []
+            for challenge_id in task.challenges:
+                challenge = EventChallenges.query.filter_by(id=challenge_id).first()
+                if challenge is None:
+                    logging.error(f"Challenge {challenge_id} not found for bingo task {task.id}.")
+                    continue
+
+                triggers = []
+                for task_id in challenge.tasks:
+                    event_task: EventTasks = EventTasks.query.filter_by(id=task_id).first()
+                    if event_task is None:
+                        logging.error(f"Event task {task_id} not found for challenge {challenge_id}.")
+                        continue
+                    for trigger_id in event_task.triggers:
+                        event_trigger: EventTriggers = EventTriggers.query.filter_by(id=trigger_id).first()
+                        if event_trigger is None:
+                            logging.error(f"Event trigger {trigger_id} not found for task {task_id}.")
+                            continue
+                        triggers.append(event_trigger.trigger)
+                
+                challenges_list.append(triggers)
+
+            response["board"][-1]["tasks"].append({
+                "task": "Task 1",
+                "required": challenge_id,
+                "id": str(task.id),
+                "triggers": challenges_list
+            })
+
+    return jsonify(response), 200   
     # Return the event data field
     return jsonify(event.data), 200
