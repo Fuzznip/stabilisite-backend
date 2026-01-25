b809e501c04f392c8ba250404b0b3a6cdb41f68d
Tboodle <Tbernstein10@gmail.com>
2026-01-25 09:20:26 -0500

    comment out alembic version table

--- scripts/drop_alembic_version.py
+++ scripts/drop_alembic_version.py
@@ -18,27 +18,28 @@ DATABASE_URL = "postgresql://{}:{}@{}".format(
 )
 
 def drop_alembic_version_table():
-    try:
-        # Create database engine
-        engine = create_engine(DATABASE_URL)
-        with engine.connect() as connection:
-            logger.info("Dropping table 'alembic_version'...")
-            connection.execute(text("DROP TABLE IF EXISTS alembic_version"))
-            connection.commit()
-            logger.info("Table 'alembic_version' dropped successfully.")
+    logger.info('skipping for now...')
+    # try:
+    #     # Create database engine
+    #     engine = create_engine(DATABASE_URL)
+    #     with engine.connect() as connection:
+    #         logger.info("Dropping table 'alembic_version'...")
+    #         connection.execute(text("DROP TABLE IF EXISTS alembic_version"))
+    #         connection.commit()
+    #         logger.info("Table 'alembic_version' dropped successfully.")
 
-            # Verify the table has been dropped
-            result = connection.execute(
-                text("SELECT to_regclass('public.alembic_version')")
-            ).scalar()
-            if result is None:
-                logger.info("Verification successful: 'alembic_version' table does not exist.")
-            else:
-                logger.warning("Verification failed: 'alembic_version' table still exists.")
-    except Exception as e:
-        logger.error(f"Error dropping table 'alembic_version': {e}")
-    finally:
-        engine.dispose()
+    #         # Verify the table has been dropped
+    #         result = connection.execute(
+    #             text("SELECT to_regclass('public.alembic_version')")
+    #         ).scalar()
+    #         if result is None:
+    #             logger.info("Verification successful: 'alembic_version' table does not exist.")
+    #         else:
+    #             logger.warning("Verification failed: 'alembic_version' table still exists.")
+    # except Exception as e:
+    #     logger.error(f"Error dropping table 'alembic_version': {e}")
+    # finally:
+    #     engine.dispose()
 
 if __name__ == "__main__":
     drop_alembic_version_table()
