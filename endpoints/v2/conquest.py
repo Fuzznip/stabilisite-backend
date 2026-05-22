8eaa7fc373a636a84193374b2de65785817cbd8f
Tboodle <Tbernstein10@gmail.com>
2026-05-21 21:35:00 -0400

    add player names to event logs

--- endpoints/v2/conquest.py
+++ endpoints/v2/conquest.py
@@ -5,7 +5,8 @@ import queue
 from app import app, db
 from flask import Response, jsonify, request, stream_with_context
 from helper.helpers import ModelEncoder
-from models.new_events import Challenge, ChallengeStatus, Event, EventLog, Region, Team, Territory
+from models.models import Users
+from models.new_events import Action, Challenge, ChallengeProof, ChallengeStatus, Event, EventLog, Region, Team, Territory
 from services.conquest_service import sse_clients
 
 
@@ -177,6 +178,78 @@ def get_territory_progress(territory_id):
             'required': challenge.quantity,
             'completions': quantity // challenge.quantity,
         })
+
+    return jsonify({'data': data}), 200
+
+
+@app.route('/v2/territories/<territory_id>/proofs', methods=['GET'])
+def get_territory_proofs(territory_id):
+    territory = Territory.query.get(territory_id)
+    if not territory:
+        return jsonify({'error': 'Territory not found'}), 404
+
+    region = Region.query.get(territory.region_id)
+    _, err = _require_conquest_event(str(region.event_id))
+    if err:
+        return err
+
+    if not territory.challenge_id:
+        return jsonify({'data': []}), 200
+
+    team_id = request.args.get('team_id')
+
+    query = ChallengeStatus.query.filter_by(challenge_id=territory.challenge_id)
+    if team_id:
+        query = query.filter_by(team_id=team_id)
+    statuses = query.all()
+
+    status_ids = [cs.id for cs in statuses]
+    all_proofs = ChallengeProof.query.filter(
+        ChallengeProof.challenge_status_id.in_(status_ids)
+    ).all() if status_ids else []
+
+    action_ids = [p.action_id for p in all_proofs if p.action_id]
+    all_actions = Action.query.filter(Action.id.in_(action_ids)).all() if action_ids else []
+    actions_by_id = {a.id: a for a in all_actions}
+
+    player_ids = [a.player_id for a in all_actions if a.player_id]
+    all_players = Users.query.filter(Users.id.in_(player_ids)).all() if player_ids else []
+    players_by_id = {p.id: p for p in all_players}
+
+    proofs_by_status = {}
+    for proof in all_proofs:
+        sid = proof.challenge_status_id
+        if sid not in proofs_by_status:
+            proofs_by_status[sid] = []
+        proofs_by_status[sid].append(proof)
+
+    data = []
+    for cs in statuses:
+        for proof in proofs_by_status.get(cs.id, []):
+            proof_dict = {
+                'id': str(proof.id),
+                'img_path': proof.img_path,
+                'created_at': proof.created_at.isoformat(),
+                'team_id': str(cs.team_id),
+            }
+            action = actions_by_id.get(proof.action_id)
+            if action:
+                proof_dict['action'] = {
+                    'id': str(action.id),
+                    'name': action.name,
+                    'source': action.source,
+                    'type': action.type,
+                    'quantity': action.quantity,
+                    'value': action.value,
+                    'date': action.date.isoformat() if action.date else None,
+                }
+                player = players_by_id.get(action.player_id)
+                if player:
+                    proof_dict['action']['player'] = {
+                        'id': str(player.id),
+                        'runescape_name': player.runescape_name,
+                    }
+            data.append(proof_dict)
 
     return jsonify({'data': data}), 200
 
